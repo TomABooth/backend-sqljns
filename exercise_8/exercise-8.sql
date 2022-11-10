b2e83c6bd49a848aead72dd6a2bcfb98e68b3507
Tom Booth <tombeezle@gmail.com>
2022-11-09 20:37:36 -0800

    exercise 9 passing, went back to fix 8 cause i looked at my terminal wrong and it was in fact not passing last test

--- exercise_8/exercise-8.sql
+++ exercise_8/exercise-8.sql
@@ -8,7 +8,8 @@ FROM
     LEFT JOIN rental ON inventory.inventory_id = rental.inventory_id
     LEFT JOIN customer ON rental.customer_id = customer.customer_id
     LEFT JOIN address ON customer.address_id = address.address_id
-    LEFT JOIN city ON city.country_id = country.country_id
+    LEFT JOIN city ON address.city_id = city.city_id
+    LEFT JOIN country on city.country_id = country.country_id
 
 WHERE
     country.country = 'Peru'
