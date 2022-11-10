b86f099960e7a1ffe40c94722323a1fe5a393cf2
Tom Booth <tombeezle@gmail.com>
2022-11-10 10:48:29 -0800

    last 3 exercises finished

--- exercise_16/exercise_16.sql
+++ exercise_16/exercise_16.sql
@@ -1,3 +1,13 @@
 -- return the title, description, release year and list of actors returned as a JSON blob
 -- for the movie Airplane Sierra
-    
+    SELECT
+        film.title, film.description, film.release_year,
+        coalesce(json_agg(to_jsonb (actor))) AS actors
+    FROM
+        film
+        LEFT JOIN film_actor on film_actor.film_id = film.film_id
+        LEFT JOIN actor on actor.actor_id = film_actor.actor_id
+    WHERE
+        film.title = 'Airplane Sierra'
+    GROUP BY
+        film.title, film.description, film.release_year
